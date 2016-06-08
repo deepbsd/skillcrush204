3af1e0b83c38d3e972e033c0d89e187e4a5e3370
David S. Jackson <deepbsd@yahoo.com>
2016-06-08 13:10:26 -0400

    more challenges to add...

--- challenges/sinatra_ar/sinatra_mvc/app/models/person.rb
+++ challenges/sinatra_ar/sinatra_mvc/app/models/person.rb
@@ -1,6 +1,6 @@
 class Person < ActiveRecord::Base
     def self.get_birthpath_num(birthdate)
-        number = birthdate[0].to_i + birthdate[1].to_i + birthdate[2].to_i + birthdate[3].to_i + birthdate[4].to_i + birthdate[5].to_i + birthdate[6].to_i + birthdate[7].to_i +
+        number = birthdate[0].to_i + birthdate[1].to_i + birthdate[2].to_i + birthdate[3].to_i + birthdate[4].to_i + birthdate[5].to_i + birthdate[6].to_i + birthdate[7].to_i
 
       	number = number.to_s
       	number = number[0].to_i + number[1].to_i
